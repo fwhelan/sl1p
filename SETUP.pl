fdd88d6d28be5397e819dcea6b5927214990a6e5
fwhelan <fiona.jane.whelan@gmail.com>
2017-03-21 18:22:20 -0400

    first commit

---
+++ SETUP.pl
@@ -0,0 +1,34 @@
+# /usr/bin/perl -w
+#Author: Fiona J Whelan <whelanfj@mcmaster.ca>
+#Last Modified Date: March 21 2017
+
+$bin = $ENV{'SL1P_BIN'};
+
+chdir($bin);
+#install local cutadapt
+print "###################INSTALLING CUTADAPT v1.8.1###################\n";
+if (! -e 'cutadapt-1.8.1.tar.gz') {
+	`wget https://pypi.python.org/packages/source/c/cutadapt/cutadapt-1.8.1.tar.gz`;
+}
+`tar -zxvf cutadapt-1.8.1.tar.gz`;
+#install local AbundantOTU+
+print "###################INSTALLING ABUNDANTOTU+ v0.93b###################\n";
+if (! -e 'get.php?justdoit=yes&software=AbundantOTUplus0.93b.tar.gz') {
+	print "wget http://omics.informatics.indiana.edu/mg/get.php\?justdoit\=yes\&software\=AbundantOTUplus0.93b.tar.gz\n";
+	`wget http://omics.informatics.indiana.edu/mg/get.php?justdoit=yes\\&software=AbundantOTUplus0.93b.tar.gz`;
+}
+print "tar -zxvf get.php?justdoit=yes&software=AbundantOTUplus0.93b.tar.gz\n";
+`tar -zxvf get.php?justdoit=yes\\&software=AbundantOTUplus0.93b.tar.gz`;
+chdir('AbundantOTU+0.93b');
+`./install`;
+chdir('..');
+#install local sickle
+print "###################INSTALLING SICKLE###################\n";
+if (! -e 'master.zip') {
+	print "wget https://github.com/najoshi/sickle/archive/master.zip\n";
+	`wget https://github.com/najoshi/sickle/archive/master.zip`;
+}
+print "tar -zxvf master.zip\n";
+`tar -zxvf master.zip`;
+chdir('..');
+exit;
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Surette laboratory 16S rRNA gene sequencing pipeline #
+##(sl1p, pronounced `slip'). ##
+
+-For information on installation and requirements, see `INSTALL.txt`.
+
+-For a comprehensive manual of sl1p, see `README.txt`.
